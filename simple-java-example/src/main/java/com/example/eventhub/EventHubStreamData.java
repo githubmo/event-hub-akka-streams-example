004d2056ec951de4894f0dfe0047b11a7c8cd0f5
Mohammed Abdulrazeg <github@mzawi.me>
2022-08-23 16:54:06 +0100

    making sure an event cannot be bigger than a megabyte

--- simple-java-example/src/main/java/com/example/eventhub/EventHubStreamData.java
+++ simple-java-example/src/main/java/com/example/eventhub/EventHubStreamData.java
@@ -3,6 +3,8 @@ package com.example.eventhub;
 import java.util.Optional;
 
 public record EventHubStreamData(byte[] bytes, Optional<String> partitionKey) {
+
+    private static final int MEGA_BYTE = 1024*1024;
     public EventHubStreamData {
         if (bytes.length <= 0) {
             throw new IllegalArgumentException("bytes sent to Event Hub cannot be empty");
@@ -10,6 +12,10 @@ public record EventHubStreamData(byte[] bytes, Optional<String> partitionKey) {
 
         if (partitionKey.isPresent() && (partitionKey.get() == null || partitionKey.get().isBlank())) {
             throw new IllegalArgumentException("partitionKey cannot contain a null or blank string");
+        }
+
+        if (bytes.length >= MEGA_BYTE) {
+            throw new IllegalArgumentException("event cannot be bigger than a megabyte");
         }
     }
 }
