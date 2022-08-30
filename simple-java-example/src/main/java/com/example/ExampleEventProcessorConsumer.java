2fc392a016cbfc59d114b37b6ad1931ff506e66c
Mohammed Abdulrazeg <github@mzawi.me>
2022-08-31 00:05:47 +0100

    wip

---
+++ simple-java-example/src/main/java/com/example/ExampleEventProcessorConsumer.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import akka.actor.ActorSystem;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import com.azure.messaging.eventhubs.models.EventContext;
+import com.example.config.AppConfig;
+import com.example.eventhub.EventHubAsyncFactory;
+import com.example.eventhub.EventHubStreamConsumer;
+import com.example.eventhub.EventHubStreamProducer;
+import com.example.eventhub.EventProcessorConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+public class ExampleEventProcessorConsumer {
+    private static final Logger logger = LoggerFactory.getLogger("ExampleEventProcessorConsumer");
+
+    public static void main(String[] args) {
+        var eventHubAsyncFactory = new EventHubAsyncFactory(AppConfig.eventHubConfig);
+        var eventHubStreamProducer = new EventHubStreamProducer(eventHubAsyncFactory.producer);
+        var eventHubStreamConsumer = new EventHubStreamConsumer(eventHubAsyncFactory.consumer);
+
+        var system = ActorSystem.create();
+
+        var sink = Flow.of(EventContext.class)
+                .throttle(1, Duration.ofSeconds(3))
+                .flatMapConcat(e ->
+                    Source.single(e.getEventData().getBodyAsString())
+                            .map(message -> {
+                                System.out.println(message);
+                                return message;
+                            })
+                            .map(message -> {
+                                logger.info("Committing a message");
+                                e.updateCheckpoint();
+                                return message;
+                            })
+                ).toMat(Sink.ignore(), Keep.right());
+
+        var eventProcessorConsumer = new EventProcessorConsumer(
+               AppConfig.eventHubConfig,
+               AppConfig.blobStorageConfig,
+               sink,
+               system
+        );
+
+        eventProcessorConsumer.start();
+        system.scheduler().scheduleOnce(Duration.ofSeconds(30), eventProcessorConsumer::stop, system.dispatcher());
+//        system.terminate();
+//        system.getWhenTerminated().toCompletableFuture().join();
+//        System.exit(0);
+    }
+}
--- simple-java-example/src/main/java/com/example/eventhub/EventProcessorConsumer.java
+++ simple-java-example/src/main/java/com/example/eventhub/EventProcessorConsumer.java
@@ -1,5 +1,11 @@
 package com.example.eventhub;
 
+import akka.Done;
+import akka.actor.ActorRef;
+import static akka.pattern.Patterns.ask;
+import akka.actor.ClassicActorSystemProvider;
+import akka.stream.CompletionStrategy;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import com.azure.messaging.eventhubs.EventProcessorClient;
 import com.azure.messaging.eventhubs.EventProcessorClientBuilder;
@@ -9,10 +15,12 @@ import com.azure.storage.blob.BlobContainerAsyncClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.example.config.BlobStorageConfig;
 import com.example.config.EventHubConfig;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
 public class EventProcessorConsumer {
 
@@ -25,27 +33,62 @@ public class EventProcessorConsumer {
 
    private EventProcessorClient eventProcessorClient;
 
+   private Sink<EventContext, CompletionStage<Done>> sink;
 
-   private EventProcessorConsumer(EventHubConfig eventHubConfig, BlobStorageConfig blobStorageConfig) {
+   private ClassicActorSystemProvider system;
+
+   private ActorRef actorRef;
+
+   public EventProcessorConsumer(EventHubConfig eventHubConfig,
+                                  BlobStorageConfig blobStorageConfig,
+                                  Sink<EventContext, CompletionStage<Done>> sink,
+                                  ClassicActorSystemProvider system
+                                  ) {
+
       this.blobContainerAsyncClient = new BlobContainerClientBuilder()
               .connectionString(blobStorageConfig.connectionString())
               .containerName(blobStorageConfig.container())
               .sasToken(blobStorageConfig.sasToken())
               .buildAsyncClient();
       this.eventHubConfig = eventHubConfig;
-      this.eventProcessorClient =
-              new EventProcessorClientBuilder()
-                      .connectionString(eventHubConfig.connectionString())
-                      .consumerGroup(eventHubConfig.consumerGroup())
-                      .processEvent()
-                      .processPartitionClose()
-                      .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient))
-                      .buildEventProcessorClient();
-
-      Source.actorRef()
+      this.sink = sink;
+      this.system = system;
    }
 
 
-   private class StreamFeeder extends AbstractBehavior
+   private Source<EventContext, ActorRef> source =
+           Source.<EventContext>actorRefWithBackpressure(
+                   "ack",
+                   // complete when we send "complete"
+                   o -> {
+                      if (o == "complete") return Optional.of(CompletionStrategy.draining());
+                      else return Optional.empty();
+                   },
+                   // do not fail on any message
+                   o -> Optional.empty());
+
+   public void start() {
+      this.actorRef = source.to(sink).run(system);
+      logger.info("Starting the EventProcessorClient");
+      this.eventProcessorClient = new EventProcessorClientBuilder()
+              .connectionString(eventHubConfig.connectionString())
+              .eventHubName(eventHubConfig.eventHubName())
+              .consumerGroup(eventHubConfig.consumerGroup())
+              .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient))
+              .processEvent(eventContext -> {
+                 ask(this.actorRef, eventContext, Duration.ofSeconds(10));
+                 logger.info("Sent an event context");
+              })
+              .processError(errorContext -> {
+                 logger.error("EventProcessorClient had a hiccup", errorContext.getThrowable());
+              })
+              .buildEventProcessorClient();
+   }
+
+   public void stop() {
+      logger.info("Stopping the EventProcessorClient and stream");
+      this.eventProcessorClient.stop();
+      this.actorRef.tell("complete", ActorRef.noSender());
+   }
 
 }
